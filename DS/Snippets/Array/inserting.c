84d4efefcf05e24c762f7fa70d381e9a1d83b21a
Kushagra <ay85166@gmail.com>
2025-10-01 11:31:11 +0530

    Oct 1 11:31

--- DS/Snippets/Array/inserting.c
+++ DS/Snippets/Array/inserting.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
 int a[100];
+int b[100];
 
-void insertback(int val, int* upper_bound , int* lower_bound){
+// inserting at the back
+void insertback(int val, int* lower_bound , int* upper_bound){
     if (*lower_bound==-1 && *upper_bound==-1){
         (*lower_bound) +=1;
         (*upper_bound) +=1;
-        a[*upper_bound]=val;
+        b[*upper_bound]=val;
     }
     else{
         *upper_bound+=1;
-        a[*upper_bound]=val;
+        b[*upper_bound]=val;
+    }
+}
+
+//inserting at the front or in the middle 
+// btw this can work for inserting at the back too if you give position pos=upper_bound+1;
+//! here the pos is sequential position and not index 
+void insert(int val , int pos , int* lower_bound , int* upper_bound){
+
+    int i = (*upper_bound)+1; // no. of elements = upper_bound +1
+    while (i>=pos){
+        b[i]=b[i-1];
+        i--;
+    }
+    *upper_bound+=1;
+    b[pos-1]=val;
+
+    for (int i=*lower_bound ; i<=*upper_bound ; i++){
+        printf("%d ",b[i]);
     }
 }
 
 int main(){
+
     // inserting at the back
     int lower_bound=-1 , upper_bound=-1; 
     for (int i=0 ;i<5 ; i++){
-    insertback(i,&upper_bound,&lower_bound);
+        insertback(i,&lower_bound,&upper_bound);
     }
+
+    // printf("%d %d\n",lower_bound,upper_bound);
+    for (int i=lower_bound ; i<=upper_bound ; i++){
+        printf("%d ",b[i]);
+    }
+    printf("\n");
 
     //inserting in the middle
+    insert(10,4,&lower_bound,&upper_bound); //! here the pos is sequential position and not index 
 
-
-    // printing the array
-    printf("%d %d\n",lower_bound,upper_bound);
-    for (int i=lower_bound ; i<=upper_bound ; i++){
-        printf("%d ",a[i]);
-    }
 
 }   
